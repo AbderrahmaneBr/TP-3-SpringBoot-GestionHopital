cf08657ca9cb01f1d3892af693ad0c51e6c99bb8
Abderrahmane Bourri <abderrahmanebourri03@gmail.com>
2025-03-17 09:35:32 +0000

    Init Project

---
+++ src/main/java/org/example/gestionhopit/GestionHopitApplication.java
@@ -0,0 +1,13 @@
+package org.example.gestionhopit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionHopitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionHopitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gestionhopit/GestionHopitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gestionhopit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionHopitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
